5fdfd1e2fc7e61541a00284a3cb564b8d528b72b
Cody W <cody@codywhitby.net>
2016-05-11 13:59:22 -0400

    rming more

--- library/bones.php
+++ library/bones.php
@@ -192,37 +192,6 @@ function bones_theme_support() {
 	    'admin-preview-callback' => ''
 	    )
 	);
-
-	// rss thingy
-	add_theme_support('automatic-feed-links');
-
-	// to add header image support go here: http://themble.com/support/adding-header-background-image-support/
-
-	// adding post format support
-	add_theme_support( 'post-formats',
-		array(
-			'aside',             // title less blurb
-			'gallery',           // gallery of images
-			'link',              // quick link to other site
-			'image',             // an image
-			'quote',             // a quick quote
-			'status',            // a Facebook like status update
-			'video',             // video
-			'audio',             // audio
-			'chat'               // chat transcript
-		)
-	);
-
-	// wp menus
-	add_theme_support( 'menus' );
-
-	// registering wp3+ menus
-	register_nav_menus(
-		array(
-			'main-nav' => __( 'The Main Menu', 'bonestheme' ),   // main nav in header
-			'footer-links' => __( 'Footer Links', 'bonestheme' ) // secondary nav in footer
-		)
-	);
 } /* end bones theme support */
 
 
